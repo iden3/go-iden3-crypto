fb1d25298f545c79bdf8173f2e83bc808e15df61
Oleksandr Brezhniev <oleksandr.brezhniev@gmail.com>
2024-02-16 02:30:42 +0000

    Add one more test vector for TestHashWithState

--- poseidon/poseidon_test.go
+++ poseidon/poseidon_test.go
@@ -138,6 +138,17 @@ func TestHashWithState(t *testing.T) {
 	b4 := big.NewInt(4)
 	b5 := big.NewInt(5)
 	b6 := big.NewInt(6)
+	b7 := big.NewInt(7)
+	b8 := big.NewInt(8)
+	b9 := big.NewInt(9)
+	b10 := big.NewInt(10)
+	b11 := big.NewInt(11)
+	b12 := big.NewInt(12)
+	b13 := big.NewInt(13)
+	b14 := big.NewInt(14)
+	b15 := big.NewInt(15)
+	b16 := big.NewInt(16)
+	b17 := big.NewInt(17)
 
 	h, err := HashWithState([]*big.Int{b1, b2, b3, b4, b5, b6}, initState0)
 	assert.Nil(t, err)
@@ -149,6 +160,12 @@ func TestHashWithState(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t,
 		"1569211601569591254857354699102545060324851338714426496554851741114291465006",
+		h.String())
+
+	h, err = HashWithState([]*big.Int{b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16}, b17)
+	assert.Nil(t, err)
+	assert.Equal(t,
+		"7865037705064445207187340054656830232157001572238023180016026650118519857086",
 		h.String())
 }
 
